cbb9837ecc83dda1da3ef68fba705dd27f91a988
maksimrepkov <maksimrepkov@yandex.ru>
2023-10-18 21:55:50 +0300

    Добавил решение задачи 27 из ДЗ к семинару 4

---
+++ Task002/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 27. 
+// Напишите программу, которая принимает на вход число и выдает сумму цифр в числе.
+
+static int FindSum(int value)
+{
+    int res = 0;
+    while (value > 0)
+    {
+        res = res + value % 10;
+        value = value / 10;
+    }
+    return res;
+}
+Console.WriteLine("Введите число: ");
+int number = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+int sum = FindSum(number);
+Console.WriteLine($"Сумма цифр в числе {number} = {sum}");
