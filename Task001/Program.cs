f7f8e60faa43c6a4b840f75147b680c762dcf56c
maksimrepkov <maksimrepkov@yandex.ru>
2023-10-18 21:49:57 +0300

    Добавил решение задачи 25 из ДЗ к семинару 4

---
+++ Task001/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 25.
+// Напишите цикл, который принимает на вход 2 числа (A и B) и возводит A в натуральную степень B.
+
+static int Pow(int val1, int val2)
+{
+    int res = 1;
+    for (int i = 1; i <= val2; i++)
+    {
+        res = res * val1;
+    }
+    return res;
+}
+Console.WriteLine("Введите первое число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int B = Convert.ToInt32(Console.ReadLine());
+int C = Pow(A, B);
+Console.WriteLine($"Результат: {C}");
